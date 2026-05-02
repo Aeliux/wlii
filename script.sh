7b7fcb5cac13056bb530ba0f99cfe480380d069c
Alireza Poodineh <itsaeliux@gmail.com>
2026-05-02 16:26:54 +0330

    Try to download gitea docs
    Signed-off-by: Alireza Poodineh <itsaeliux@gmail.com>

--- script.sh
+++ script.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
 
-gh release view -R nextcloud/android
+set +e
+
+mkdir -p downloads/gitea-docs downloads/gitea-docs-wget
+
+sudo apt install -y httrack
+
+httrack "https://docs.gitea.com/" -O downloads/gitea-docs -*
+
+cd downloads/gitea-docs-wget || exit 1
+wget -m -H -k -K "https://docs.gitea.com/"
